77c312541bd2696d0eff0490b7a9deb583a0fac6
Gianluigi Silvestri <gian@192.168.0.104>
2020-12-29 15:03:27 +0100

    added results loader

--- results_loader.py
+++ results_loader.py
@@ -1,7 +1,53 @@
 import pickle
+import os
+from scipy.stats import sem
+import numpy as np
+from shutil import copyfile
 
-pickle_in = open("uni_results.pickle","rb")
-uni_results = pickle.load(pickle_in)
+model_name = "lz"
+lik_name = "c"
+exp_name = model_name + "_" + lik_name
 
-pickle_in = open("multi_results.pickle","rb")
-multi_results = pickle.load(pickle_in)
+results = {}
+
+
+pickle_in = open(f"{exp_name}_results/uni_results.pickle", "rb")
+results['uni'] = pickle.load(pickle_in)
+
+pickle_in = open(f"{exp_name}_results/multi_results.pickle", "rb")
+results['multi'] = pickle.load(pickle_in)
+
+pickle_in = open(f"{exp_name}_results/pred_results.pickle", "rb")
+results['pred'] = pickle.load(pickle_in)
+
+for k, models in results.items():
+    print(f'{k}')
+    s = ''
+    for m, values in models.items():
+        s+=f'{m}'
+        for v in values:
+            s+=f' & {v:.3f}'
+        s += f"\\\\\n"
+
+    print(s)
+
+for k, models in results.items():
+    print(f'{k}')
+    s = ''
+    for m, values in models.items():
+        s+=f'{m}'
+        s += f' & {np.mean(values):.3f} $\\pm$ {sem(values):.3f}'
+        s += f"\\\\\n"
+
+    print(s)
+
+if not os.path.isdir(f'{exp_name}_selected_figures'):
+    os.makedirs(f'{exp_name}_selected_figures')
+for m, values in results['uni'].items():
+    max_v = np.argmax(values)
+    for r in range(3):
+        file_name=f'{m}_rep:{max_v}_{r}.png'
+        copyfile(f'{exp_name}_figures/{file_name}', f'{exp_name}_selected_figures/{file_name}')
+
+
+
